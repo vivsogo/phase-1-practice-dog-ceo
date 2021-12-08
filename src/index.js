92fe71798ce2ca2fbeaf726b32a77c425cf619ed
Vivian Gomez <viviangomez0@gmail.com>
2021-12-08 14:25:33 -0500

    commit tree 80b017cab0ed2e1e9e85b8ffa6157bb7e32b3cc7

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 console.log('%c HI', 'color: firebrick')
+//the event takes two arguments the event and the function we are 
+//also grabbing by the id setting up  a listener event 
+// we are trying to grab this to append the images because thats where they are located 
+document.addEventListener("DOMContentLoaded", function(){
+    let dogImageContainer = document.getElementById('dog-image-container')
+    
+
+
+// we will use fetch to grab those images.
+//fetch is a promise. change the promise object by 
+//using .then 
+//the image is in the Json and we need to grab it 
+ fetch("https://dog.ceo/api/breeds/image/random/4")
+ .then(response=> response.json())
+ .then(function(jsonObject){
+     let arrayOfDogURLs =jsonObject.message
+     arrayOfDogURLs.forEach(url=>{
+         dogImageContainer.innerHTML += makeImagetagString(url)
+     })
+         
+ })
+
+})
+//we need to itrate the array we do this using foreach
+//the make image returns a string will take one url and be embbeded into image
+
+function makeImagetagString(url){
+    return `<img src="${url}"/>`
+
+}
